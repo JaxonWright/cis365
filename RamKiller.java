fcc0221a45cbb9827dd6d3d1020e929ec5a7ca21
Jaxon Wright <jaxonhwright@gmail.com>
2016-02-03 00:40:32 -0500

    Added safeBack(), which will keep robot from hitting walls. Currently broken, needs more debugging :P

--- RamKiller.java
+++ RamKiller.java
@@ -9,7 +9,9 @@ public class RamKiller extends AdvancedRobot
 {
 	int backCount = 0; // used to change direction after a certain amount of back() calls.
 					   // This helps from getting jammed between wall and RamFire.
-	double backupDistance = 110; //default amount to back up
+	final double MAX_BACKUP = 110; //max amount to back up
+	final double FIELD_PADDING = 50; //padding to sat within (mainly to avoid walls entirely)
+	
 	/**
 	 * run: RamKiller's default behavior
 	 */
@@ -32,15 +34,59 @@ public class RamKiller extends AdvancedRobot
 		} else {
 		  if (backCount<2) {
 		  	backCount++; //if we've backed up a small amoutn of times
-			back(backupDistance);
+			safeBack();
 		  } else {
-		  	turnRight(90); //otherwires, change direction
-			back(20);
+		  	turnRight(90); //otherwise, change direction
+			safeBack();
 		  	backCount = 0;
 		  }
-		  
 		  fire(3);
 		}	
+	}
+	
+	/**
+	 * safeBack: safely back up within boundaries set to avoid walls entirely
+	 */
+	void safeBack() {
+		double backAmount = MAX_BACKUP;
+		double desiredX;
+		double desiredY;
+		double rightBound = getBattleFieldWidth() - FIELD_PADDING;
+		double topBound = getBattleFieldHeight() - FIELD_PADDING;
+		double heading = getHeading()-180; //we are backing up
+		double hRad = Math.toRadians(heading);
+		//Figure out desired X and Y values
+		desiredX = Math.asin(hRad)*backAmount;
+		desiredY = Math.acos(hRad)*backAmount;
+		
+		System.out.println("Back Heading: " + heading);
+		System.out.println("Current X: " + getX());
+		System.out.println("Current Y: " + getY());
+		
+		System.out.println("Desired X: " + desiredX);
+		System.out.println("Desired Y: " + desiredY);
+		
+		//adjust desiredX
+		if (desiredX <= FIELD_PADDING){ //too colse to adjusted left wall
+			desiredX = FIELD_PADDING;
+		} else if ( desiredX >= rightBound){ //too colse to adjusted right wall
+			desiredX = rightBound;
+		}
+		//adjust desiredY
+		if (desiredY <= FIELD_PADDING){ //too close to adjusted bottom wall
+			desiredY = FIELD_PADDING;
+		} else if (desiredY >= topBound){ //too close to adjusted top wall
+			desiredY = topBound;
+		}
+		
+		System.out.println("New Desired X: " + desiredX);
+		System.out.println("New Desired Y: " + desiredY);
+		//backAmount to the distance between current point and new point
+		//										          ______________________
+		//This is simply the standard distance formula, -/ (y2-y1)^2 + (x2-x1)^2
+		backAmount = Math.sqrt(Math.pow(desiredY-getY(), 2) + Math.pow(desiredX-getX(), 2));
+		System.out.println("Back Amount = " + backAmount);
+		back(backAmount);
 	}
 	
 	/**
@@ -64,11 +110,12 @@ public class RamKiller extends AdvancedRobot
 	 * onHitByBullet: What to do when you're hit by a bullet
 	 */
 	public void onHitByBullet(HitByBulletEvent e) {
-		back(100);
+		//back(100);
 	}
 	
 	/**
 	 * onHitWall: What to do when you hit a wall
+	 * THIS SHOULD NEVER HAPPEN
 	 */
 	public void onHitWall(HitWallEvent e) {
 		ahead(10); //move up so rotating doesn't cause another wall hit
